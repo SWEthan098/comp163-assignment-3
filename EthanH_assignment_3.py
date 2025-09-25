747b8114c466c6006a7d7e4a0dd36013a01be93f
SWEthan098 <colealexander07@gmail.com>
2025-09-25 19:41:06 -0400

    Pass Test Case 1: Initial game setup with required variables

---
+++ EthanH_assignment_3.py
@@ -0,0 +1,7 @@
+student_name = "Ethan"  # Replace with your actual name
+current_gpa = 4.0 # Float between 1.0-4.0
+study_hours = 31 # Integer (Ex. 25)
+social_points = 45# Integer (Ex. 50)
+stress_level = 75  # Integer 0-100
+#Initializes the foundation of the simulator
+print(f"Welcome {student_name}, here are your current stats:\nGPA: {current_gpa}\nStudy Hours: {study_hours}\nSocial Points: {social_points}\nStress Level: {stress_level}")
